43955386ca4cc12bb508ebe529dcb1257569ade4
PiotrTra <ponczuszek363@gmail.com>
2023-12-14 21:09:38 +0100

    added reading high score from file to save the high score after restarting the game

--- scoreboard.py
+++ scoreboard.py
@@ -8,8 +8,10 @@ class Scoreboard(Turtle):
 
     def __init__(self):
         super().__init__()
+        
         self.score = 0
-        self.high_score = 0
+        with open("highscore.txt")as data:
+            self.high_score = int(data.read())
         self.goto(0, 270)
         self.ht()
         self.penup()
@@ -22,6 +24,8 @@ class Scoreboard(Turtle):
 
     def reset(self):
         if self.score > self.high_score:
+            with open("highscore.txt", mode="w") as data: 
+                data.write(str(self.score))
             self.high_score = self.score
         self.score = 0
         self.update_scoreboard()
